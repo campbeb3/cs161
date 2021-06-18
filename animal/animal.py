645fbbb715b7b76503a3c1bc2f40a3859b051e36
bmcampbe <campbeb3@oregonstate.edu>
2021-06-17 17:46:57 -0700

    committing

---
+++ animal/animal.py
@@ -0,0 +1,7 @@
+# Author: Brian Campbell
+# Date: 6/17/21
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
